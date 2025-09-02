341c6fcc4a4be1668483c2fef04d0ddb369f7e0b
김주홍 <gicente1@nate.com>
2025-09-02 23:36:56 +0900

    fix

--- practice/Assets/JsonTest3.cs
+++ practice/Assets/JsonTest3.cs
@@ -15,7 +15,6 @@ public class FrameData
 {
     public float timeStamp;
     public Vector3 position;
-    public Quaternion rotation;
 }
 
 public class JsonTest3 : MonoBehaviour
@@ -24,25 +23,57 @@ public class JsonTest3 : MonoBehaviour
     public static string FileFullPath => Path.Combine(Application.persistentDataPath, fileName);
 
     public GameObject playerObject;
-    public float recordingInterval = 0.1f;
+    private float recordingInterval = 0.1f;
+    private float recordingTime = 0f;
+    private float lastRecordTime = 0f;
+
+    bool isRecording = false;
+    bool isPlaying = false;
 
     private RecordingData currentRecording = new RecordingData();
 
     void Update()
     {
+        if(isRecording)
+        {
+            recordingTime += Time.deltaTime;
+            if (recordingTime - lastRecordTime >= recordingInterval)
+            {
+                FrameData frame = new FrameData()
+                {
+                    timeStamp = recordingTime,
+                    position = playerObject.transform.position
+                };
+                currentRecording.frames.Add(frame);
+                lastRecordTime = recordingTime;
+            }
+        }
+
+        if (isPlaying)
+        {
+        }
 
     }
 
     public void Rec()
     {
-        var json = JsonConvert.SerializeObject(currentRecording, new Vector3Converter());
-        File.WriteAllText(FileFullPath, json);
+        if (isRecording)
+        {
+            var json = JsonConvert.SerializeObject(currentRecording, new Vector3Converter());
+            File.WriteAllText(FileFullPath, json);
+            isRecording = false;
+        }
+        else
+        {
+            isRecording = true;
+            recordingTime = 0f;
+        }
     }
 
     public void Play()
     {
             var json = File.ReadAllText(FileFullPath);
             currentRecording = JsonConvert.DeserializeObject<RecordingData>(json, new Vector3Converter());
-        
+            isPlaying = true;
     }
 }
